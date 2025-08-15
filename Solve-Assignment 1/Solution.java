4a7244a2130d357d1b99284896f2341160939d1c
MD. RAHATUL ISLAM RAHAT <rahatulislam476@gmail.com>
2025-08-15 15:42:18 +0600

    Create Solution.java

---
+++ Solve-Assignment 1/Solution.java
@@ -0,0 +1,14 @@
+C. Fast Sum
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            long N = sc.nextLong();
+            System.out.println(N * (N + 1) / 2);
+        }
+    }
+}
+
